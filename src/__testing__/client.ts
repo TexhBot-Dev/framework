4c0734c812161ac11320f406f1e0aa83f61d83db
Scott Gould <greysilly7@gmail.com>
2022-07-25 19:18:04 -0400

    wooops

--- src/__testing__/client.ts
+++ src/__testing__/client.ts
@@ -1,12 +1,9 @@
 import { GatewayIntentBits } from "discord.js";
 import { TechClient } from "..";
-// import { token } from "./config.json";
+import { token } from "./config.json";
 
 const client = new TechClient({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
 
-client.login("OTc3NzgzOTAyNTA2NzM3NjY0.GzHfBO.eBWQLJw8WKFJEsjxsnc_-V1UKCnj3qWzjkDjGk").then(() => {
-	console.log(client.commands);
-});
-
+client.login(token);
