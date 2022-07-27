a115669fc9e3e19234009946c4a72ec21c15ae3c
Scott Gould <greysilly7@gmail.com>
2022-07-27 18:47:49 -0400

    Remove maxlen ignore stuff

--- src/helpers/paths/getSourceDir.ts
+++ src/helpers/paths/getSourceDir.ts
@@ -1,6 +1,5 @@
 import {existsSync} from 'node:fs';
 
-// eslint-disable-next-line max-len
 /** The cached source directory, so we don't need to access the file system every time. */
 let sourceDir: string;
 
@@ -18,13 +17,11 @@ export function getSourceDir() {
   const dist = cwd + '/dist';
   if (existsSync(dist)) return (sourceDir = dist);
 
-  // eslint-disable-next-line max-len
   // Fallback to src folder for non-TypeScript users, non-TypeScript users may choose to use dist folder instead however
   const src = cwd + '/src';
   if (existsSync(src)) return (sourceDir = src);
 
   throw new Error(
-      // eslint-disable-next-line max-len
       'Could not find source directory! Please ensure if you are using TypeScript, you have a dist folder for transpiled files. If you are using JavaScript, please make a src or dist for your JavaScript files.',
   );
 }
