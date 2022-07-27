0fabd471fe949a963b5d868d479451002e311368
greysilly7 <greysilly7@gmail.com>
2022-07-27 17:09:33 -0400

    Update walk.ts

--- src/helpers/fs/walk.ts
+++ src/helpers/fs/walk.ts
@@ -10,10 +10,10 @@ export async function walk(dir: string, filter?: RegExp): Promise<string[]> {
   const dirEntries = await opendir(dir);
   for await (const dirEntry of dirEntries) {
     if (dirEntry.isFile()) {
-      if (filter && filter.test(dirEntry.name)) continue;
+      if (filter && !filter.test(dirEntry.name)) continue;
       files.push(dir + '/' + dirEntry.name);
     } else if (dirEntry.isDirectory()) {
-      files.push(...await walk(dir + '/' + dirEntry.name));
+      files.push(...await walk(dir + '/' + dirEntry.name, filter));
     }
   }
   return files;
